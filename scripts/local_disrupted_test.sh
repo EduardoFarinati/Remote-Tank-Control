3cfea94e7a2d4016d3fa8e72d56f65af2409e97e
Eduardo Farinati Leite <eduardo.farinati@ufrgs.br>
2022-04-02 21:26:55 -0300

    Add new cli arguments to local_disrupted_test script

--- scripts/local_disrupted_test.sh
+++ scripts/local_disrupted_test.sh
@@ -9,7 +9,7 @@ make debug
 ./scripts/udp-lossy-relay.py 127.0.0.1:9696 127.0.0.1:9595
 
 # Run server
-gnome-terminal -- gdb ./debug/bin/server
+gnome-terminal -- gdb ./debug/bin/server -p 9696 -a 127.0.0.1:9595
 
 # Run client
-gnome-terminal -- gdb ./debug/bin/client
+gnome-terminal -- gdb ./debug/bin/client -p 9595
