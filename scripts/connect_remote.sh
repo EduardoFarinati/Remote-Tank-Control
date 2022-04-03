bd4f9e549d6365ad15535064ab934319ce995e38
Eduardo Farinati Leite <eduardo.farinati@ufrgs.br>
2022-04-03 08:14:06 -0300

    Add XForwarding to connect_remote

--- scripts/connect_remote.sh
+++ scripts/connect_remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Remote internal passphrase: pass12
-ssh -o 'PubkeyAcceptedKeyTypes +ssh-rsa' eng10048_5@pandora.ece.ufrgs.br -i ~/ufrgs_protocolos_key
+ssh -o 'PubkeyAcceptedKeyTypes +ssh-rsa' -Y eng10048_5@pandora.ece.ufrgs.br -i ~/ufrgs_protocolos_key
